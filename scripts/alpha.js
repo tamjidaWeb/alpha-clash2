5170fc3cf2d1489e30585205e49904879d1c59e6
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-09-06 13:29:19 +0600

    commited

---
+++ scripts/alpha.js
@@ -0,0 +1,26 @@
+// function play(){
+//     //hide the home screen to hinde the screen add the class hidden to the home section
+
+    
+//     const homeScreen = document.getElementById('home-screen');
+//     homeScreen.classList.add('hidden');
+
+//     //show the playground
+//     const playGround = document.getElementById('play-ground');
+//     playGround.classList.remove('hidden');
+// }
+function continueGame(){
+
+    //step 1: generate a random alphabet
+    
+
+}
+
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame();
+}
+
+
+
---
+++ scripts/utility.js
@@ -0,0 +1,10 @@
+function hideElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden')
+}
+
+function showElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden');
+
+}
